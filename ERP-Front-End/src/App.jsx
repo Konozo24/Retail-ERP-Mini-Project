1461feb5985b4115678880e0a8d002af5dd95420
Justin 0906 <justinwong0609@gmail.comgit config --global user.name>
2025-12-10 17:32:13 +0800

    j

--- ERP-Front-End/src/App.jsx
+++ ERP-Front-End/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// src/App.jsx
+
+// 1. Only import React/Hooks if you are using them (useState is now removed)
+// 2. Import the main page component that contains the application's layout
+import Products from './Pages/Products';
 import './App.css'
-import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // All the unused imports (logos, useState, count) are removed.
+  
+  // Return the Products component to render the RetailFlow interface
   return (
-    <span className='text-accent'>DEEE</span>
+    <Products />
   )
 }
 
