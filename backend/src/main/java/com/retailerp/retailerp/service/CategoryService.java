94342018405086d44936a4e8236f9cca14fe6b68
Konozo24 <mingwei12242016@gmail.com>
2025-12-22 17:38:36 +0800

    fix(category): fix issue where category is not completely deleted after user delete it

--- backend/src/main/java/com/retailerp/retailerp/service/CategoryService.java
+++ backend/src/main/java/com/retailerp/retailerp/service/CategoryService.java
@@ -17,58 +17,65 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
-    
-    private final CategoryRepository categoryRepository;
 
-    @Transactional(readOnly = true)
-    public List<CategoryDTO> getCategories() {
-        return categoryRepository.findAll()
-            .stream()
-            .map(CategoryDTO::fromEntity)
-            .toList();
-    }
+	private final CategoryRepository categoryRepository;
 
-    @Transactional(readOnly = true)
-    public CategoriesNameDTO getCategoriesName() {
-        return CategoriesNameDTO.fromEntity(categoryRepository.findAll());
-    }
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> getCategories() {
+		return categoryRepository.findAll()
+				.stream()
+				.filter(category -> !category.isInactive())
+				.map(CategoryDTO::fromEntity)
+				.toList();
+	}
 
-    @Transactional(readOnly = true)
-    public CategoryDTO getCategory(Long categoryId) {
-        Category category = categoryRepository.findById(categoryId).orElseThrow(
-                () -> new NoSuchElementException("Category with id, " + categoryId + " doesn't exist!"));
-        return CategoryDTO.fromEntity(category);
-    }
+	@Transactional(readOnly = true)
+	public CategoriesNameDTO getCategoriesName() {
+		List<Category> activeCategories = categoryRepository.findAll()
+				.stream()
+				.filter(category -> !category.isInactive())
+				.toList();
+		return CategoriesNameDTO.fromEntity(activeCategories);
+	}
 
-    @Transactional
-    public CategoryDTO createCategory(CategoryRequestDTO request) {
-        Category category = new Category(request.getName(), request.getPrefix());
-        if (request.getColor() != null) category.setColor(request.getColor());
-        if (request.getImage() != null) category.setImage(request.getImage());
-        return CategoryDTO.fromEntity(categoryRepository.save(category));
-    }
+	@Transactional(readOnly = true)
+	public CategoryDTO getCategory(Long categoryId) {
+		Category category = categoryRepository.findById(categoryId).orElseThrow(
+				() -> new NoSuchElementException("Category with id, " + categoryId + " doesn't exist!"));
+		return CategoryDTO.fromEntity(category);
+	}
 
-    @Transactional
-    public void updateCategory(Long categoryId, CategoryRequestDTO request) {
-        Category existing = categoryRepository.findById(categoryId).orElseThrow(
-                () -> new NoSuchElementException("Category with id, " + categoryId + " doesn't exist!"));
+	@Transactional
+	public CategoryDTO createCategory(CategoryRequestDTO request) {
+		Category category = new Category(request.getName(), request.getPrefix());
+		if (request.getColor() != null)
+			category.setColor(request.getColor());
+		if (request.getImage() != null)
+			category.setImage(request.getImage());
+		return CategoryDTO.fromEntity(categoryRepository.save(category));
+	}
 
-        existing.setName(request.getName());
-        existing.setPrefix(request.getPrefix().toUpperCase());
-        existing.setColor(request.getColor() != null ? request.getColor() : existing.getColor());
-        existing.setImage(request.getImage() != null ? request.getImage() : existing.getImage());
-        categoryRepository.save(existing);
-    }    
+	@Transactional
+	public void updateCategory(Long categoryId, CategoryRequestDTO request) {
+		Category existing = categoryRepository.findById(categoryId).orElseThrow(
+				() -> new NoSuchElementException("Category with id, " + categoryId + " doesn't exist!"));
 
-    @Transactional
-    public void removeCategory(Long categoryId) {
-        Category existing = categoryRepository.findById(categoryId).orElseThrow(
-                () -> new NoSuchElementException("Category with id, " + categoryId + " doesn't exist!"));
+		existing.setName(request.getName());
+		existing.setPrefix(request.getPrefix().toUpperCase());
+		existing.setColor(request.getColor() != null ? request.getColor() : existing.getColor());
+		existing.setImage(request.getImage() != null ? request.getImage() : existing.getImage());
+		categoryRepository.save(existing);
+	}
 
-        if (!existing.isInactive()) {
-            existing.setInactive(true);
-            categoryRepository.save(existing);
-        }
-    }
+	@Transactional
+	public void removeCategory(Long categoryId) {
+		Category existing = categoryRepository.findById(categoryId).orElseThrow(
+				() -> new NoSuchElementException("Category with id, " + categoryId + " doesn't exist!"));
+
+		if (!existing.isInactive()) {
+			existing.setInactive(true);
+			categoryRepository.save(existing);
+		}
+	}
 
 }
